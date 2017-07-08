6c80bdf5355f72b4658c410aad1de797c0350803
John A. Fedoruk <johnny@johnfedoruk.ca>
2017-07-08 11:30:11 +0100

    Generate key pair

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { pki } from "node-forge";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+	title = 'app works!';
+	keypair:{pub:string,priv:string};
+	t:{start?:Date,end?:Date};
+	ngOnInit() {
+		this.t = {};
+		this.t.start = new Date();
+		pki.rsa.generateKeyPair(2048,this.keysGenerated.bind(this));
+	}
+	keysGenerated(e,kp:pki.KeyPair) {
+		if(e)
+			console.warn(e);
+		this.keypair = {
+			priv:pki.privateKeyToPem(kp.privateKey).trim(),
+			pub:pki.publicKeyToPem(kp.publicKey).trim()
+		}
+		this.t.end = new Date();
+		console.log(`${Math.round((this.t.end.getTime() - this.t.start.getTime()))}ms to generate key pair`);
+	}
 }
